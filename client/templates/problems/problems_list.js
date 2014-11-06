554ccb994445b35ef4b655edb8b21a5b5c1a2e7e
Srinivas Mangipudi <shrinimann@gmail.com>
2014-11-06 22:15:14 +0530

    added fetching from collection

--- client/templates/problems/problems_list.js
+++ client/templates/problems/problems_list.js
@@ -1,18 +1,5 @@
-var problemsData = [
-	{
-		title: 'Introducing My City My Problems',
-		url: 'http://sachagreif.com/introducing-telescope/'
-	},
-	{
-		title: 'Meteor',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		url: 'http://themeteorbook.com'
-	}
-];
-	
 Template.problemsList.helpers({
-	problems: problemsData
+	problems: function(){
+		return Problems.find();
+	}
 });
---
+++ server/fixtures.js
@@ -0,0 +1,16 @@
+if (Problems.find().count() === 0) {
+	Problems.insert({
+	title: 'Potholed roads',
+	url: 'http://timesofindia.indiatimes.com/city/chennai/Potholed-roads-pose-a-deadly-threat-to-seniors/articleshow/44966477.cms'
+	});
+
+	Problems.insert({
+	title: 'Garbage in my neighbourhood not being collected',
+	url: 'http://www.wsbtv.com/news/news/local/trash-piles-local-apartment-complex-despite-tenant/nh2Mr/'
+	});
+
+	Problems.insert({
+	title: 'Traffic congestion increasing pollution',
+	url: 'http://www.coventrytelegraph.net/news/coventry-news/coventrys-bus-lanes-could-scrapped-8055383'
+	});
+}
