5e78c9bf454319a6df737846afc097ebcaff9e62
Srinivas Mangipudi <shrinimann@gmail.com>
2014-11-14 15:37:32 +0530

    added problem edit/delete owned by user.

--- client/templates/problems/problem_item.js
+++ client/templates/problems/problem_item.js
@@ -1,4 +1,8 @@
 Template.problemItem.helpers({
+	ownProblem: function() {
+		return this.userId === Meteor.userId();
+	},
+
 	domain: function() {
 		var a = document.createElement('a');
 		a.href = this.url;
